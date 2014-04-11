38c687a4808a2329c275781e32367b1aab94ce5b
taoeaten <taoeaten@163.com>
2014-04-11 10:17:18 +0800

    switch to less

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
